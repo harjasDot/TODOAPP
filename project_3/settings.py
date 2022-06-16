8a68f28877a032381532681e419fc0371b3d1f6d
Harjaspreet singh <singh.harjas2002@gmail.com>
2022-06-16 20:54:03 +0530

    dd

--- project_3/settings.py
+++ project_3/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9x4njb&#m73$c%n89d(1jis^9^ev+@91fxq_m3$jf!86q1)gq8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','to-do-list1232']
+ALLOWED_HOSTS = ['127.0.0.1','to-do-list1232.herokuapp.com/']
 
 
 # Application definition
